b7eb0b289fa676e8a898b52fc55e68e1f6ff381f
Aryan <aryansajan630@gmail.com>
2024-05-05 22:33:32 +0530

    Updated recipe app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import AddRecipie from './components/AddRecipe';
+import AddRecipe from './components/AddRecipe';
 
 function App() {
   return (
     <div>
-      <AddRecipie/>
+      <AddRecipe/>
     </div>
   );
 }
